e6415d2f43e611aa3ddd0608544d1a6c6b157fff
Sebastian <alzate.sebastian@correounivalle.edu.co>
2024-04-08 21:05:20 -0500

    segundo cambio en rama de pruebas

--- logica.js
+++ logica.js
@@ -1,5 +1,6 @@
-alert("mimamameamamimamamemima")
+alert("Taller Tecnologico II")
 console.log("esto que estoy escribiendo se verá en la consola de html")
 console.log("Esta va a ser la tercera version")
 console.log("Este cambio irá hasta remoto")
 console.log("Se agrega cambio en rama de pruebas")
+console.log("Segundo cambio en rema de pruebas")
