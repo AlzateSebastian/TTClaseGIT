6f12446236f6be0fc7f39ec7b04d035e789ecfb7
Sebastian <alzate.sebastian@correounivalle.edu.co>
2024-04-08 20:06:17 -0500

    Se agrega un cambio para ver en remoto

--- logica.js
+++ logica.js
@@ -1,3 +1,4 @@
 alert("mimamameamamimamamemima")
 console.log("esto que estoy escribiendo se verá en la consola de html")
 console.log("Esta va a ser la tercera version")
+console.log("Este cambio irá hasta remoto")
